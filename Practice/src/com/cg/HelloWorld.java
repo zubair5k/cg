0d0a76dee922c70529ca95790c77d9b2fb4ba84b
shaikh <shaikh@ZANDR>
2020-04-15 11:15:53 +0530

    Initial commit

---
+++ Practice/src/com/cg/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+/**
+ * My Hello World class
+ * @author Zubair Shaikh
+ * @version 1.0
+ */
+public class HelloWorld implements Hello {
+	
+	@Override
+	public String sayHello() {
+		return "Hello World!";
+	}
+}
